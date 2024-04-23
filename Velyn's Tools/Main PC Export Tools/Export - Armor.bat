ed8236de93e1041abba8c70edd48d54867a51e78
Shiyo <strikinginsanity@hotmail.com>
2024-04-22 21:33:43 -0400

    Velyn' tools

---
+++ Velyn's Tools/Main PC Export Tools/Export - Armor.bat
@@ -0,0 +1,3 @@
+copy "C:\Ashita 4\polplugins\DATs\ArmorDats\ROM\0\7.DAT" "C:\Ashita 4\polplugins\DATs\ArmorDats\ROM\0\7_backup.DAT"
+copy "C:\Ashita 4\polplugins\DATs\ArmorDats\ROM\118\109.DAT" "C:\Ashita 4\polplugins\DATs\ArmorDats\ROM\118\109_backup.DAT"
+json2item.exe "C:\Ashita 4\polplugins\DATs\armor.json" "C:\Ashita 4\polplugins\DATs\ArmorDats\ROM\0\7.DAT" "C:\Ashita 4\polplugins\DATs\ArmorDats\ROM\118\109.DAT" 
